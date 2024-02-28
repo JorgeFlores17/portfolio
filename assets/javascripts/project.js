cb762556ff604bff1535f81c81cd822417dc1b42
JorgeFlores17 <george1999flowers@gmail.com>
2024-02-27 17:59:51 -0700

    Se agrega otra imagen para project

--- assets/javascripts/project.js
+++ assets/javascripts/project.js
@@ -31,7 +31,7 @@ const projects = [
   },
   {
     title: "Exercise Tracker",
-    cardImage: "assets/images/project-page/exercise.jpg",
+    cardImage: "assets/images/project-page/studin.gif",
     description: "Exercise tracker built using basic redux.",
     tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
     Previewlink: "",
